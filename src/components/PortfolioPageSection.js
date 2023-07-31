f2a5a5b4caa1c9722cb0af8e62aa02522d340e43
emergency22 <alex.d.murillo@protonmail.com>
2023-07-31 12:53:55 -0500

    Add text overlay

--- src/components/PortfolioPageSection.js
+++ src/components/PortfolioPageSection.js
@@ -6,16 +6,43 @@ export default function PortfolioPageSection() {
             <div className="portfolioStyle">
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 1 " /><br />
+                    <div className="techStackOverlay">
+                        <ul>
+                            <li>Technology 1</li>
+                            <li>Technology 2</li>
+                            <li>Technology 3</li>
+                            <li>Technology 4</li>
+                            <li>Technology 5</li>
+                        </ul>
+                    </div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 2 " /><br />
+                    <div className="techStackOverlay">
+                        <ul>
+                            <li>Technology 1</li>
+                            <li>Technology 2</li>
+                            <li>Technology 3</li>
+                            <li>Technology 4</li>
+                            <li>Technology 5</li>
+                        </ul>
+                    </div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 3 " /><br />
+                    <div className="techStackOverlay">
+                        <ul>
+                            <li>Technology 1</li>
+                            <li>Technology 2</li>
+                            <li>Technology 3</li>
+                            <li>Technology 4</li>
+                            <li>Technology 5</li>
+                        </ul>
+                    </div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
@@ -24,16 +51,43 @@ export default function PortfolioPageSection() {
             <div className="portfolioStyle">
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 4 " /><br />
+                    <div className="techStackOverlay">
+                        <ul>
+                            <li>Technology 1</li>
+                            <li>Technology 2</li>
+                            <li>Technology 3</li>
+                            <li>Technology 4</li>
+                            <li>Technology 5</li>
+                        </ul>
+                    </div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 5 " /><br />
+                    <div className="techStackOverlay">
+                        <ul>
+                            <li>Technology 1</li>
+                            <li>Technology 2</li>
+                            <li>Technology 3</li>
+                            <li>Technology 4</li>
+                            <li>Technology 5</li>
+                        </ul>
+                    </div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 6 " /><br />
+                    <div className="techStackOverlay">
+                        <ul>
+                            <li>Technology 1</li>
+                            <li>Technology 2</li>
+                            <li>Technology 3</li>
+                            <li>Technology 4</li>
+                            <li>Technology 5</li>
+                        </ul>
+                    </div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
