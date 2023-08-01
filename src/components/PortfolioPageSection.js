177b7c69cd23d6204c4074c0e319f8275a6bd361
gguio <gdguio@outlook.com>
2023-08-01 14:15:26 -0400

    Latest Changes

--- src/components/PortfolioPageSection.js
+++ src/components/PortfolioPageSection.js
@@ -1,4 +1,5 @@
 import Image1 from '../images/200x300.png'
+import { projectMap } from '../data/database'
 
 export default function PortfolioPageSection() {
     return (
@@ -8,11 +9,9 @@ export default function PortfolioPageSection() {
                     <img src={Image1} alt="Image 1 " /><br />
                     <div className="techStackOverlay">
                         <ul>
-                            <li>Technology 1</li>
-                            <li>Technology 2</li>
-                            <li>Technology 3</li>
-                            <li>Technology 4</li>
-                            <li>Technology 5</li>
+                            <li>{projectMap.get("Awesome Project").technologies[0]}</li>
+                            <li>{projectMap.get("Awesome Project").technologies[1]}</li>
+                            <li>{projectMap.get("Awesome Project").technologies[2]}</li>
                         </ul>
                     </div>
                     <a href="">Video Demo</a><br />
