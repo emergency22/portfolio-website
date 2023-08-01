55c89c7ddb796c6db1eb3b8409c905a32d648c3e
emergency22 <alex.d.murillo@protonmail.com>
2023-08-01 14:01:21 -0500

    Save point

--- src/components/PortfolioPageSection.js
+++ src/components/PortfolioPageSection.js
@@ -1,22 +1,33 @@
 import Image1 from '../images/200x300.png'
-import { projectMap } from '../data/database'
+// import { projectMap } from '../data/database'
+
+// var array = [zebra, lion, tiger, bear];
+
+// function iterateArray() {
+//     for (var i=0; i > array.length; i++) {
+//         return (array[i]);
+//     }
+// }
 
 export default function PortfolioPageSection() {
     return (
         <div className="portfolioPageSection">
             <div className="portfolioStyle">
+                <div className="portfolioItem">
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 1 " /><br />
                     <div className="techStackOverlay">
                         <ul>
-                            <li>{projectMap.get("Awesome Project").technologies[0]}</li>
-                            <li>{projectMap.get("Awesome Project").technologies[1]}</li>
-                            <li>{projectMap.get("Awesome Project").technologies[2]}</li>
+                            {/* console.log("projectMap", projectMap); */}
+                            {/* <li>{projectMap.get("Awesome Project").technologies[0]}</li> */}
+                            {/* <li>{projectMap.get("Awesome Project").technologies[1]}</li>
+                            <li>{projectMap.get("Awesome Project").technologies[2]}</li> */}
                         </ul>
-                    </div>
+                    </div></div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
+                <div className="portfolioItem">
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 2 " /><br />
                     <div className="techStackOverlay">
@@ -27,10 +38,11 @@ export default function PortfolioPageSection() {
                             <li>Technology 4</li>
                             <li>Technology 5</li>
                         </ul>
-                    </div>
+                    </div></div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
+                <div className="portfolioItem">
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 3 " /><br />
                     <div className="techStackOverlay">
@@ -41,13 +53,14 @@ export default function PortfolioPageSection() {
                             <li>Technology 4</li>
                             <li>Technology 5</li>
                         </ul>
-                    </div>
+                    </div></div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
             </div>
 
             <div className="portfolioStyle">
+                <div className="portfolioItem">
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 4 " /><br />
                     <div className="techStackOverlay">
@@ -58,10 +71,11 @@ export default function PortfolioPageSection() {
                             <li>Technology 4</li>
                             <li>Technology 5</li>
                         </ul>
-                    </div>
+                    </div></div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
+                <div className="portfolioItem">
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 5 " /><br />
                     <div className="techStackOverlay">
@@ -72,10 +86,11 @@ export default function PortfolioPageSection() {
                             <li>Technology 4</li>
                             <li>Technology 5</li>
                         </ul>
-                    </div>
+                    </div></div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
+                <div className="portfolioItem">
                 <div className="portfolioImageItem">
                     <img src={Image1} alt="Image 6 " /><br />
                     <div className="techStackOverlay">
@@ -86,7 +101,7 @@ export default function PortfolioPageSection() {
                             <li>Technology 4</li>
                             <li>Technology 5</li>
                         </ul>
-                    </div>
+                    </div></div>
                     <a href="">Video Demo</a><br />
                     <a href="">GitHub Repo</a>
                 </div>
