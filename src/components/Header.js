fdba47d025d2f1fb9b8e6ed5d1a45fee47a1c84c
emergency22 <alex.d.murillo@protonmail.com>
2023-08-10 15:05:53 -0500

    omg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
+// import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+
 export default function Header() {
+    const scrollToAboutMe = () => {
+        const aboutMeSection = document.getElementById('aboutMe');
+        if (aboutMeSection) {
+            aboutMeSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+    
     return (
     <section class="header">
         <nav>
             <div class="nav-links" id="navLinks">
                 <ul>
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/home/#aboutMe">About Me</a></li>
+                    <li><NavLink to="/home">Home</NavLink></li>
+                    <li><a href="#aboutMe" onClick={scrollToAboutMe}>About Me</a></li>
                     <li><a href="https://docs.google.com/document/d/1I6TqZTnOhw0Nhjnft9ptlRIm3yZOQQpp/edit?usp=sharing&ouid=105921216387970791018&rtpof=true&sd=true" target="_blank">Resume</a></li>
-                    <li><a href="/portfolio">Portfolio</a></li>
+                    <li><NavLink to="/portfolio">Portfolio</NavLink></li>
                     <li><a href="https://github.com/emergency22" target="_blank">GitHub</a></li>
                     <li><a href="https://www.linkedin.com/in/alexdmurillo/" target="_blank">LinkedIn</a></li>
                 </ul>
--- src/components/PortfolioIntroduction.js
+++ src/components/PortfolioIntroduction.js
@@ -1,12 +1,11 @@
-// import { Link } from 'react-router-dom'; // Import Link from react-router-dom if you're using it for routing
+import { Link } from 'react-router-dom';
 
 export default function PortfolioIntroduction() {
-
-    return (
-        <div className="portfolioIntroduction">
-            <button onClick={() => window.location.href = "/portfolio"} className="portfolio-button">
-                View My Portfolio
-            </button>
-        </div>
-    );
+  return (
+    <div className="portfolioIntroduction">
+      <Link to="/portfolio" className="portfolio-button">
+        View My Portfolio
+      </Link>
+    </div>
+  );
 }
